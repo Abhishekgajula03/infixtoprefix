d56f7544da00161380aa58a25b325da18152aaab
Abhishek Gajula <117979265+Abhishekgajula03@users.noreply.github.com>
2023-10-26 18:56:57 +0530

    Create infixtopostfix.cpp

---
+++ infixtopostfix.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int prec(char c) {
+  if (c == '^')
+    return 3;
+  else if (c == '/' || c == '*')
+    return 2;
+  else if (c == '+' || c == '-')
+    return 1;
+  else
+    return -1;
+}
+
+void infix_to_postfix(string s){
+    stack<char>st;
+    string res;
+    for(int i=0;i<s.length();i++){
+        char c=s[i];
+        if(c=='('){
+            st.push(c);
+        }
+         else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+            res+=c;
+         }
+        else if(c==')'){
+            while(st.top()!='('){
+                res+=st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else{
+            while(!st.empty()&&prec(s[i])<=prec(st.top())){
+                res+=st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+    while(!st.empty()){
+        res+=st.top();
+        st.pop();
+    }
+    cout<<res;
+
+}
+
+int main(){
+    string s="a+b*c+(w/r)";
+    infix_to_postfix(s);
+}
